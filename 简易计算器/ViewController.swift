abfad2d0dbe687e1133233a5361cce109ab6e52a
S0daWh1skey <albertsphepherd@alberts-macbook-pro.local>
2017-02-21 16:40:36 +0800

    Initial Commit

---
+++ 简易计算器/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  简易计算器
+//
+//  Created by Albert Sphepherd on 2017/2/21.
+//  Copyright © 2017年 Albert Sphepherd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
